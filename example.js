99aa5145e6357b02dff4ba8b93338912f7694f17
Mohammed Al qurafi <15172611+mhmd-22@users.noreply.github.com>
2020-10-28 14:12:06 +0300

    update example

--- example.js
+++ example.js
@@ -7,24 +7,30 @@ const config = {
 
 async function main() {
 	try {
-		// Configs are passed to DocumentClient
 		// New table will be created if not exists
+		// configs are passed to DocumentClient
 		// Don't forget to check region also
 		const db = await new DynamoKVStore("test", config);
 
-		db.cacheTimeout = 50;
+		db.cacheTimeout = 1000;
 
 		// the value should be a valid DynamoDB Attribute
-		await db.set("foo", "bar");
+		await db.set("foo", { hello: "world", key: "value" });
 
 		let v = await db.get("foo");
 		console.log(v);
 
-		// or directly
-		console.log(await db.foo);
+		setTimeout(async () => {
+			console.log(await db.foo);
 
-		await db.delete("foo");
-		// or db.remove
+			//or directly
+			console.log(await db.foo);
+
+			await db.delete("foo");
+			// or db.remove
+
+			console.log(await db.get("foo")); // undefined
+		}, 2000);
 	} catch (e) {
 		console.error(e);
 	}
